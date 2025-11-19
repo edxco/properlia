56063afbf524b99dda6632e0e8abfe85d75ee48a
Edxco <eduardo.baeza@yahoo.com>
2025-11-19 16:10:03 -0600

    feat(frontend): install tailwindcss

--- packages/frontend/tailwind.config.js
+++ packages/frontend/tailwind.config.js
@@ -3,6 +3,8 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: { extend: {} },
+  theme: {
+    extend: {},
+  },
   plugins: [],
-}
+};
