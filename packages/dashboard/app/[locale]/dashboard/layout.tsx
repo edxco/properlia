89eb80dc811bae6cc863472f9a3acfd1f5a6fba6
Edxco <eduardo.baeza@yahoo.com>
2026-01-12 17:05:20 -0600

    Increase dashboard content area max-width

--- packages/dashboard/app/[locale]/dashboard/layout.tsx
+++ packages/dashboard/app/[locale]/dashboard/layout.tsx
@@ -47,7 +47,7 @@ export default function DashboardLayout({
       </header>
 
       {/* Main Content */}
-      <main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 py-8">
+      <main className="mx-auto max-w-9/10 px-4 sm:px-6 lg:px-8 py-8">
         {children}
       </main>
     </div>
