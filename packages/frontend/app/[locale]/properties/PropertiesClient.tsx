e646e409f3e527d9f1a89021dfebd5d8cea9aebd
Edxco <eduardo.baeza@yahoo.com>
2026-01-25 15:28:35 -0600

    feat(frontend): add banners to properties page

--- packages/frontend/app/[locale]/properties/PropertiesClient.tsx
+++ packages/frontend/app/[locale]/properties/PropertiesClient.tsx
@@ -7,7 +7,7 @@ import { NoResultsAlert } from "@/components/alerts/NoResultsAlert";
 import { useProperties } from "@/src/services/properties/queries";
 import { useT } from "@properlia/shared/components/TranslationProvider";
 import { Property } from "@properlia/shared/types";
-import { PropertyCard } from "@/components/ui";
+import { PropertyCard, Banner } from "@/components/ui";
 
 interface Filters {
   rooms?: number;
@@ -144,6 +144,27 @@ export default function PropertiesClient() {
           cities={cities}
           states={states}
         />
+
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 my-6">
+          <Banner
+            title={t("bannerSearchTitle")}
+            description={t("bannerSearchDescription")}
+            icon={
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                <path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd" />
+              </svg>
+            }
+          />
+          <Banner
+            title={t("bannerGuidanceTitle")}
+            description={t("bannerGuidanceDescription")}
+            icon={
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                <path d="M10.394 2.08a1 1 0 00-.788 0l-7 3a1 1 0 000 1.84L5.25 8.051a.999.999 0 01.356-.257l4-1.714a1 1 0 11.788 1.838L7.667 9.088l1.94.831a1 1 0 00.787 0l7-3a1 1 0 000-1.838l-7-3zM3.31 9.397L5 10.12v4.102a8.969 8.969 0 00-1.05-.174 1 1 0 01-.89-.89 11.115 11.115 0 01.25-3.762zM9.3 16.573A9.026 9.026 0 007 14.935v-3.957l1.818.78a3 3 0 002.364 0l5.508-2.361a11.026 11.026 0 01.25 3.762 1 1 0 01-.89.89 8.968 8.968 0 00-5.35 2.524 1 1 0 01-1.4 0zM6 18a1 1 0 001-1v-2.065a8.935 8.935 0 00-2-.712V17a1 1 0 001 1z" />
+              </svg>
+            }
+          />
+        </div>
 
         {hasNoResults && (
           <NoResultsAlert
