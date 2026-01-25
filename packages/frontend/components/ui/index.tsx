25ea9ddc7c01a17613cf01123476ab6d2beedd39
Edxco <eduardo.baeza@yahoo.com>
2026-01-25 15:28:10 -0600

    feat(frontend): add Banner component

--- packages/frontend/components/ui/index.tsx
+++ packages/frontend/components/ui/index.tsx
@@ -1,3 +1,4 @@
+export { Banner } from './Banner';
 export { PillLink } from './pillLink';
 export { PropertyCard } from './propertyCard';
 export { PropertyLabelStats } from './PropertyLabelStats';
