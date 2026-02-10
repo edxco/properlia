3b6ee9fb7d2d7c46cef8f251cc3d3af63ca1eb51
Edxco <eduardo.baeza@yahoo.com>
2026-02-09 23:03:35 -0600

    feat(home): add ServiceTiles component for buy/sell/invest CTAs

--- packages/frontend/app/[locale]/page.tsx
+++ packages/frontend/app/[locale]/page.tsx
@@ -1,13 +1,15 @@
 import { Hero } from "../../src/components/Hero";
 import { MainAbout } from "@/src/components/MainAbout";
 import { FeaturedProperties } from "@/src/components/FeaturedProperties";
+import { ServiceTiles } from "@/src/components/ServiceTiles";
 
 export default function Page() {
   return (
     <>
       <Hero />
+      <ServiceTiles />
       <FeaturedProperties />
       <MainAbout />
     </>
   );
-};
+}
